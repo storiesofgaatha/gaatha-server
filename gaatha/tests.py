0c9276a3aca2ec38975e7b2ee3d28e5473023482
rup <rup.rajbanshi@togglecorp.com>
2023-02-08 11:41:02 +0545

    Add test runner in github actions

--- gaatha/tests.py
+++ gaatha/tests.py
@@ -55,7 +55,7 @@ class TestCase(BaseTestCase):
 class FakeTest(TestCase):
     """
     This test is for running migrations only
-    docker-compose run --rm server ./manage.py test -v 2 --pattern="config/tests/test_fake.py"
+    docker-compose run --rm server ./manage.py test -v 2 --pattern="gaatha/tests.py"
     """
     def test_fake(self):
         pass
