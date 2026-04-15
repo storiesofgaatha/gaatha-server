864e4c11892f29afab4bb65823ba32ce666f7891
sudip-khanal <sudipkhanal0777@gmail.com>
2026-04-15 21:37:22 +0545

    feat(model): add secure field
    - Add custom SecureFileField and SecureImageField with uuid4
    - Replace django FileField with SecureFieldField
    - Replace django ImageField with SecureImageField

--- gaatha/utils.py
+++ gaatha/utils.py
@@ -1,4 +1,6 @@
 from strawberry.enum import EnumType
+from django.db.models.fields.files import FileField, ImageField
+from uuid import uuid4
 
 
 def get_enum_label(
@@ -9,3 +11,19 @@ def get_enum_label(
     if value:
         return enum_type(value).label
     return default_description
+
+
+class SecureFileField(FileField):
+    def generate_filename(self, instance, filename):
+        """
+        Overwrites https://github.com/django/django/blob/main/django/db/models/fields/files.py#L345
+        """
+        # Append uuid4 path to the filename
+        filename = f"{uuid4().hex}/{filename}"
+        return super().generate_filename(instance, filename)
+
+
+class SecureImageField(ImageField):
+    def generate_filename(self, instance, filename):
+        filename = f"{uuid4().hex}/{filename}"
+        return super().generate_filename(instance, filename)
